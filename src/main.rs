0eb680b3858bd9c48fc30f0aa6546a4456311f0f
= <=>
2024-02-22 19:23:14 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::asm, panic::PanicInfo, ptr::write_volatile};
+use core::{
+    panic::PanicInfo,
+    ptr::{read_volatile, write_volatile},
+};
 
 mod boot {
     use core::arch::global_asm;
 
     global_asm!(".section .start._start");
 }
-
 const GPFSEL2: *mut u32 = 0x3F20_0008 as *mut u32;
 const GPSET0: *mut u32 = 0x3F20_001C as *mut u32;
 const GPCLR0: *mut u32 = 0x3F20_0028 as *mut u32;
+
+const CLO: *mut u32 = 0x3F00_3004 as *mut u32;
+const CHI: *mut u32 = 0x3F00_3008 as *mut u32;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -22,20 +27,21 @@ pub extern "C" fn _start() -> ! {
         unsafe {
             write_volatile(GPSET0, 1 << 26);
         }
-        for _ in 0..5_000_000 {
-            unsafe {
-                asm!("nop");
-            }
-        }
+        delay(1000);
         unsafe {
             write_volatile(GPCLR0, 1 << 26);
         }
-        for _ in 0..5_000_000 {
-            unsafe {
-                asm!("nop");
-            }
-        }
-    }   
+        delay(1000);
+    }
+}
+
+fn delay(t: u32) {
+    let target = time() + (t * 1000) as u64;
+    while time() < target {}
+}
+
+fn time() -> u64 {
+    unsafe { (read_volatile(CHI) as u64) << 32 | (read_volatile(CLO) as u64) }
 }
 
 #[panic_handler]
